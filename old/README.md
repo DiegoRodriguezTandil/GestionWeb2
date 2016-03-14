8d57848e02e49e9c6be79d9c914b372db1918c7b
Diego Rodriguez <diego@qwavee.com>
2016-03-14 14:20:54 -0300

    Old Project into tar.gz file

--- old/README.md
+++
@@ -1,4 +0,0 @@
-# GestionWeb2
-CRM/ERP Software 
-Usa rdash
-https://github.com/rdash/rdash-angular
--- old/web-service/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gestionweb',
-    'username' => 'root',
-    'password' => 'mysqlpass',
-    'charset' => 'utf8',
-];
